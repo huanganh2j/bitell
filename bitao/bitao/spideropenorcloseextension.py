749a1fb1baccd70b9b2296b1e93133ec94e0729a
huangan <270753933@qq.com>
2018-03-20 14:13:46 +0800

    微博爬虫完善

--- bitao/bitao/spideropenorcloseextension.py
+++ bitao/bitao/spideropenorcloseextension.py
@@ -4,6 +4,7 @@ from scrapy import cmdline
 from twisted.internet import reactor, defer
 import time
 from scrapy.exceptions import NotConfigured
+import MySQLdb
 class SpiderOpenCloseLogging(object):
     def __init__(self):
         super(SpiderOpenCloseLogging, self).__init__()
@@ -16,6 +17,14 @@ class SpiderOpenCloseLogging(object):
 
     def spider_closed(self, spider):
         print "spider "+spider.id+"关闭信号接收"
+        conn = MySQLdb.Connect(host='localhost', port=3306, user='root', passwd='923469an', db='reptile_metadata',
+                               charset='UTF8');
+        cursor = conn.cursor()
+        # 修改处理状态
+        cursor.execute(" update weibo_target_list set is_handle=1 ")
+        conn.commit()
+        cursor.close()
+        conn.close()
         # if spider.name=="jinse":
         #     cmdline.execute(
         #         "scrapy runspider C:/Users/huangan/PycharmProjects/bitell/bitao/bitao/spiders/weibosecondspider.py".split())
